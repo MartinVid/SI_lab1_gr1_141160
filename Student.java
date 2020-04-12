592c5d6a417943b99b25937e130b426109b8fde2
MartinVid <m.vidovic.mk@gmail.com>
2020-04-12 19:14:20 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -2,16 +2,60 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
-
+	List<Integer> labPoints = new ArrayList<Integer>();
 	//TODO constructor
-
-	//TODO seters & getters
-
-	public double getAverage() {
-		//TODO
+	public Student(String index, String firstName, String lastName, List<int> labPoints) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
 	}
 
-	public boolean hasSignature() {
-		//TODO
+	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Integer> getLabPoints() {
+		return labPoints;
+	}
+
+	public void setLabPoints(List<Integer> labPoints) {
+		this.labPoints = labPoints;
+	}
+
+	public double getAverage(List <Integer> labPoints) {
+		Integer sum = 0;
+		if(!labPoints.isEmpty()) {
+			for (Integer labPoint : labPoints) {
+				sum += labPoint;
+			}
+			return sum.doubleValue() / labPoints.size();
+		}
+		return sum;
+	}
+
+	public boolean hasSignature(List <Integer> labPoints) {
+		return labPoints.size() > 8 ? true : false;
 	}
 }
